19c391b667428a5dc5aeb8ba0c25d6618ee3d5bd
Vlad Frolov <frolvlad@gmail.com>
2020-02-01 20:01:29 -0300

    added a joinNear button

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
 
   async joinNear() {
     const q = await this.props.contract.incrementParticipation();
-    console.log("QQ", q);
+    alert("You have joined! Stay tuned!");
   }
 
   render() {
@@ -85,49 +85,68 @@ class App extends Component {
         <div className="image-wrapper">
           {/*<img className="logo" src={nearlogo} alt="NEAR logo" />*/}
           {/*<h2>hmm</h2>*/}
-          <div className={'hackathon-text'}>
+          <div className={"hackathon-text"}>
             <p>
-              NEAR protocol is a new blockchain focused on developer productivity and useability! In this demonstration we're going to interact with the blockchain and see a live change on the projector.</p>
-            <p><b>So what's going to happen?</b></p>
-              <p>You will login to your new, handy-dandy NEAR account. We keep track of the number of participants who log in and update a simple value on the blockchain.</p>
-              <p><strong>How do you store this info?</strong></p>
-              <p>I'm glad you asked, for this demonstration we used AssemblyScript, but you can also use Rust.</p>
-            <p><strong>Tell me more!</strong></p>
-              <p>Sure, grab one of us at the booth, we'd love to chat!</p>
-            
+              NEAR protocol is a new blockchain focused on developer
+              productivity and useability! In this demonstration we're going to
+              interact with the blockchain and see a live change on the
+              projector.
+            </p>
+            <p>
+              <b>So what's going to happen?</b>
+            </p>
+            <p>
+              You will login to your new, handy-dandy NEAR account. We keep
+              track of the number of participants who log in and update a simple
+              value on the blockchain.
+            </p>
+            <p>
+              <strong>How do you store this info?</strong>
+            </p>
+            <p>
+              I'm glad you asked, for this demonstration we used AssemblyScript,
+              but you can also use Rust.
+            </p>
+            <p>
+              <strong>Tell me more!</strong>
+            </p>
+            <p>Sure, grab one of us at the booth, we'd love to chat!</p>
           </div>
-        <div>
-          {this.state.login ? (
-            <button onClick={this.requestSignOut}>Log out</button>
-          ) : (
-            <button onClick={this.requestSignIn}>Sure I'll try it!</button>
-          )}
-        </div>
-        {/*<div>*/}
+          <div>
+            {this.state.login ? (
+              <button onClick={this.joinNear}>Join NEAR!</button>
+            ) : null}{" "}
+            {this.state.login ? (
+              <button onClick={this.requestSignOut}>Log out</button>
+            ) : (
+              <button onClick={this.requestSignIn}>Sure I'll try it!</button>
+            )}
+          </div>
+          {/*<div>*/}
           {/*<p>*/}
-            {/*<span role="img" aria-label="net">*/}
-              {/*🕸*/}
-            {/*</span>{" "}*/}
-            {/*<a className="App-link" href="https://nearprotocol.com">*/}
-              {/*NEAR Website*/}
-            {/*</a>{" "}*/}
-            {/*<span role="img" aria-label="net">*/}
-              {/*🕸*/}
-            {/*</span>*/}
+          {/*<span role="img" aria-label="net">*/}
+          {/*🕸*/}
+          {/*</span>{" "}*/}
+          {/*<a className="App-link" href="https://nearprotocol.com">*/}
+          {/*NEAR Website*/}
+          {/*</a>{" "}*/}
+          {/*<span role="img" aria-label="net">*/}
+          {/*🕸*/}
+          {/*</span>*/}
           {/*</p>*/}
           {/*<p>*/}
-            {/*<span role="img" aria-label="book">*/}
-              {/*📚*/}
-            {/*</span>*/}
-            {/*<a className="App-link" href="https://docs.nearprotocol.com">*/}
-              {/*{" "}*/}
-              {/*Learn from NEAR Documentation*/}
-            {/*</a>{" "}*/}
-            {/*<span role="img" aria-label="book">*/}
-              {/*📚*/}
-            {/*</span>*/}
+          {/*<span role="img" aria-label="book">*/}
+          {/*📚*/}
+          {/*</span>*/}
+          {/*<a className="App-link" href="https://docs.nearprotocol.com">*/}
+          {/*{" "}*/}
+          {/*Learn from NEAR Documentation*/}
+          {/*</a>{" "}*/}
+          {/*<span role="img" aria-label="book">*/}
+          {/*📚*/}
+          {/*</span>*/}
           {/*</p>*/}
-        {/*</div>*/}
+          {/*</div>*/}
         </div>
       </div>
     );
